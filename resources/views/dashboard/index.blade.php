a246a5753c1eb591ab1343befa4370eabb7a1342
Salwa Abier Rhafa <salwaabierrhafa21@gmail.com>
2026-07-19 06:06:01 +0700

    Create dashboard controller and connect route

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="bg-white rounded-lg shadow p-6">
+
+    <h1 class="text-3xl font-bold">
+        Global Supply Chain Dashboard
+    </h1>
+
+    <p class="mt-3 text-gray-600">
+        Selamat datang di Sistem Monitoring Global Supply Chain.
+    </p>
+
+</div>
+
+@endsection
