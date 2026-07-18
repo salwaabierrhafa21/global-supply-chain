0c5fc920ddab945a5ee3a2cf2067afd8c7e40a4e
Salwa Abier Rhafa <salwaabierrhafa21@gmail.com>
2026-07-18 21:07:51 +0700

    Create weather_logs table

---
+++ database/migrations/2026_07_18_140102_create_weather_logs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('weather_logs', function (Blueprint $table) {
+
+        $table->id();
+
+        $table->foreignId('country_id')
+              ->constrained('countries')
+              ->cascadeOnDelete();
+
+        $table->decimal('temperature',5,2)->nullable();
+
+        $table->integer('humidity')->nullable();
+
+        $table->decimal('wind_speed',5,2)->nullable();
+
+        $table->string('weather',100)->nullable();
+
+        $table->string('weather_icon',20)->nullable();
+
+        $table->timestamp('recorded_at');
+
+        $table->timestamps();
+
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather_logs');
+    }
+};
